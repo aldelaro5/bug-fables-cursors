0ad780c4857d5b288f5197c65c2fe62afef93d42
aldelaro5 <aldelaro5@gmail.com>
2022-11-21 03:02:49 -0500

    initial commit

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# A script for preparing binaries for version release of Pokemon Cursors, by Abdulkaiz Khatri
+
+key="bf"
+comment="An unofficial Bug Fables cursors."
+
+# Cleanup old builds
+rm -rf themes bin &&
+ctgen build.toml -p x11 &&
+
+# Building Pokemon Windows binaries
+ctgen build.toml -p windows -s 16 -d "bitmaps/${key}" -n "${key}-Small" -c "$comment" &&
+ctgen build.toml -p windows -s 24 -d "bitmaps/${key}" -n "${key}-Regular" -c "$comment" &&
+ctgen build.toml -p windows -s 32 -d "bitmaps/${key}" -n "${key}-Large" -c "$comment" &&
+ctgen build.toml -p windows -s 48 -d "bitmaps/${key}" -n "${key}-Extra-Large" -c "$comment" &&
+
+
+# Compressing Binaries
+mkdir -p bin &&
+cd themes &&
+
+tar -czvf "../bin/${key}.tar.gz" "${key}" &&
+zip -rv "../bin/${key}-Windows.zip" "${key}-Small-Windows" "${key}-Regular-Windows" "${key}-Large-Windows" "${key}-Extra-Large-Windows" &&
+
+cd ..
