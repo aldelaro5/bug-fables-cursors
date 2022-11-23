aa34a2b4f1d02f37e9a790a7ced2685bf3182153
aldelaro5 <aldelaro5@gmail.com>
2022-11-22 19:00:04 -0500

    Fix windows build stuff

--- release.sh
+++ release.sh
@@ -9,17 +9,13 @@ rm -rf themes bin &&
 ctgen build.toml -p x11 &&
 
 # Building Pokemon Windows binaries
-ctgen build.toml -p windows -s 16 -d "bitmaps/${key}" -n "${key}-Small" -c "$comment" &&
-ctgen build.toml -p windows -s 24 -d "bitmaps/${key}" -n "${key}-Regular" -c "$comment" &&
-ctgen build.toml -p windows -s 32 -d "bitmaps/${key}" -n "${key}-Large" -c "$comment" &&
-ctgen build.toml -p windows -s 48 -d "bitmaps/${key}" -n "${key}-Extra-Large" -c "$comment" &&
-
+ctgen build.toml -p windows -s 32 -d "bitmaps/${key}" -n "${key}" -c "$comment"
 
 # Compressing Binaries
 mkdir -p bin &&
 cd themes &&
 
 tar -czvf "../bin/${key}.tar.gz" "${key}" &&
-zip -rv "../bin/${key}-Windows.zip" "${key}-Small-Windows" "${key}-Regular-Windows" "${key}-Large-Windows" "${key}-Extra-Large-Windows" &&
+zip -rv "../bin/${key}-Windows.zip" "${key}-Windows" &&
 
 cd ..
